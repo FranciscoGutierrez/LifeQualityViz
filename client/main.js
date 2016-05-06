301b92cd36353c6c60b003113666a68c15026e85
FranciscoGutierrez <fsalvador23@gmail.com>
2016-05-06 12:21:09 +0200

    Added ChartJs bar and donut

--- client/main.js
+++ client/main.js
@@ -12,49 +12,81 @@ Template.tweets.onCreated(function() {
   },500);
 });
 
+Template.sentimentmap.onCreated(function() {
+  $(document).ready(function() {
+    mymap = L.map('map').setView([40.7128, -74.0059], 12);
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+      maxZoom: 18,
+      id: 'franciscoghz.026h6h3d',
+      accessToken: 'pk.eyJ1IjoiZnJhbmNpc2NvZ2h6IiwiYSI6ImNpbnV1Z3J4eTAwb3R2c2tscXgwMWs2eHEifQ.OrlG9so4YihIM4ccw59cHw'
+    }).addTo(mymap);
+    var iconPositive = L.divIcon({className: 'tweet-icon-positive'});
+    var iconNegative = L.divIcon({className: 'tweet-icon-negative'});
+    L.marker([40.7128, -74.0059],{clickable: false, icon: iconPositive}).addTo(mymap);
+    L.marker([40.7328, -74.0059],{clickable: false, icon: iconNegative}).addTo(mymap);
+  });
+});
+
 Template.lifeIndex.onCreated(function() {
-  $( document ).ready(function() {
+  $(document).ready(function() {
+    Chart.defaults.global.responsive = false;
+    Chart.defaults.global.legend = false;
+    var data = { labels: ["",""], datasets: [{data: [125.86, 74.14], backgroundColor: ["#d1d1d1","white"], hoverBackgroundColor: ["#d1d1d1","white"]}]};
+    var options = {responsive: true};
     var ctx = document.getElementById("myChart");
-    var myChart = new Chart(ctx, {
-      type: 'bar',
-      data: {
-        backgroundColor: "#424242",
-        data: [12, 19, 3, 5, 2, 3]
-      }});
+    var donutchart = new Chart(ctx, { type: 'doughnut', data: data, options: options });
+    //////
+    var data2 = { labels: ["Safety","Health Care","Climate"], datasets: [{data: [51.16, 60.59,79.69], backgroundColor: "#d1d1d1", hoverBackgroundColor: "#d1d1d1"}]};
+    var options2 = {responsive: true};
+    var ctx2 = document.getElementById("myChart2");
+    var options2 = {
+      scales: {
+        yAxes: [{
+          display: true,
+          beginAtZero: true,
+          ticks: {
+            suggestedMin: 0,
+            suggestedMax: 100,
+            beginAtZero: true
+          }
+        }]
+      }
+    };
+    var barchart = new Chart(ctx2, { type: 'bar', data: data2, options: options2 });
+
+  });
+});
+
+Template.tweets.helpers({
+  tweets() {
+    if ($('.tweets-container').length > 10) $('.simple-tweet').slice($('.tweets-container').length-10).remove();
+    return Tweets.find({}, {
+      transform: function(item){
+        item.polarity = (item.polarity > 0) ? item.polarity = "positive" : item.polarity = "negative";
+        item.tweetdate = moment(item.tweetdate).fromNow();
+        return item;
+      }
     });
-  });
+  }
+});
 
+Template.lifeIndex.helpers({
+  currentIndex(){
+    return Tweets.findOne({}, {
+      transform: function(item){
+        item.safety_index    = item.safety_index.toFixed(2);
+        item.pollution_index = item.pollution_index.toFixed(2);
+        item.traffic_index   = item.traffic_index.toFixed(2);
+        item.quality_of_life_index = item.quality_of_life_index.toFixed(2);
+        return item;
+      }
+    },{sort: {DateTime: 1}});
+  }
+});
 
-  Template.tweets.helpers({
-    tweets() {
-      if ($('.tweets-container').length > 10) $('.simple-tweet').slice($('.tweets-container').length-10).remove();
-      return Tweets.find({}, {
-        transform: function(item){
-          item.polarity = (item.polarity > 0) ? item.polarity = "positive" : item.polarity = "negative";
-          item.tweetdate = moment(item.tweetdate).fromNow();
-          return item;
-        }
-      });
-    }
-  });
-
-  Template.lifeIndex.helpers({
-    currentIndex(){
-      return Tweets.findOne({}, {
-        transform: function(item){
-          item.safety_index    = item.safety_index.toFixed(2);
-          item.pollution_index = item.pollution_index.toFixed(2);
-          item.traffic_index   = item.traffic_index.toFixed(2);
-          item.quality_of_life_index = item.quality_of_life_index.toFixed(2);
-          return item;
-        }
-      },{sort: {DateTime: 1}});
-    }
-  });
-
-  Template.tweets.events({
-    'click button'(event, instance) {
-      // increment the counter when button is clicked
-      instance.counter.set(instance.counter.get() + 1);
-    },
-  });
+Template.tweets.events({
+  'click button'(event, instance) {
+    // increment the counter when button is clicked
+    instance.counter.set(instance.counter.get() + 1);
+  },
+});
--- server/main.js
+++ server/main.js
@@ -8,6 +8,6 @@ Meteor.publish("tweets", function () {
   //900000 15 min
   //1800000 30 min
   //3600000 1 hour
-  return Tweets.find({tweetdate: {$gte: new Date(new Date().getTime() - 900000)}});
-  //return Tweets.find({},{limit: 25});
+  //return Tweets.find({tweetdate: {$gte: new Date(new Date().getTime() - 900000)}});
+  return Tweets.find({},{sort: {tweetdate: -1}, limit: 15});
 });
