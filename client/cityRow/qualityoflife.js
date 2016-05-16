7f40e3b04de65a61c0d819ed4feee543c3452066
FranciscoGutierrez <fsalvador23@gmail.com>
2016-05-16 21:45:42 +0200

    Added figs reactivity

--- client/cityRow/qualityoflife.js
+++ client/cityRow/qualityoflife.js
@@ -6,7 +6,46 @@ Template.lifequality.helpers({
     var s = parseInt(Session.get("strength-s"));
     var p = parseInt(Session.get("strength-p"));
     var x = (h+t+s+p)/check;
-    console.log(x);
     return  x;
+  },
+  weather() {
+    var h = parseInt(Session.get("strength-h"));
+    var current = Session.get("rowtop");
+    var title = "";
+    $('paper-checkbox[checked]').each(function() {
+      var name = $(this).attr('class').split(' ')[0];
+      if(name=="health") title = current+"_w.png";
+    });
+    return {name: title, opacity:h/100};
+  },
+  safety() {
+    var s = parseInt(Session.get("strength-s"));
+    var current = Session.get("rowtop");
+    var title = "";
+    $('paper-checkbox[checked]').each(function() {
+      var name = $(this).attr('class').split(' ')[0];
+      if(name=="safety") title = current+"_s.png";
+    });
+    return {name: title, opacity:s/100};
+  },
+  traffic() {
+    var t = parseInt(Session.get("strength-t"));
+    var current = Session.get("rowtop");
+    var title = "";
+    $('paper-checkbox[checked]').each(function() {
+      var name = $(this).attr('class').split(' ')[0];
+      if(name=="traffic") title = current+"_t.png";
+    });
+    return {name: title, opacity:t/100};
+  },
+  airqual() {
+    var p = parseInt(Session.get("strength-p"));
+    var current = Session.get("rowtop");
+    var title = "";
+    $('paper-checkbox[checked]').each(function() {
+      var name = $(this).attr('class').split(' ')[0];
+      if(name=="polluted") title = current+"_a.png";
+    });
+    return {name: title, opacity:p/100};
   }
 });
