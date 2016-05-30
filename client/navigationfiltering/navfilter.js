9ff0aa92881d875a6a795dc49b9c2ad2bf771405
FranciscoGutierrez <fsalvador23@gmail.com>
2016-05-30 15:20:36 +0200

    Fixed sliders

--- client/navigationfiltering/navfilter.js
+++ client/navigationfiltering/navfilter.js
@@ -37,39 +37,47 @@ Template.navfilter.helpers({
 });
 
 Template.navfilter.events({
-  'click .health'  (event, instance) {
+  'click .health'  (event, template) {
+    var sh = Number(Session.get("strength-h"));
     if ($('.health').prop('checked')) {
       $(".im-h, .weather-info, .cr-name-h, .cityrow-health, .crh-health, .crf-health").fadeIn();
-      Session.set("strength-h",80);
+      Session.set("strength-h",Number(sh+1));
     } else {
       $(".im-h, .weather-info, .cr-name-h, .crf-health, .crh-health, .cityrow-health").fadeOut();
-      Session.set("strength-h",0);
+      Session.set("strength-h",Number(sh+1));
     }
   },
-  'click .safety'  (event, instance) {
+  'click .safety'  (event, template) {
+    var ss = Number(Session.get("strength-s"));
     if ($('.safety').prop('checked')) {
       $(".im-s, .safety-info, .cr-name-s, .crf-safety, .crh-safety, .cityrow-safety").fadeIn();
-      Session.set("strength-s",80);
+      Session.set("strength-s",Number(ss+1));
     } else {
       $(".im-s, .safety-info, .cr-name-s, .crf-safety, .crh-safety, .cityrow-safety").fadeOut();
-      Session.set("strength-s",0);
+      Session.set("strength-s",Number(ss+1));
     }
   },
-  'click .traffic' (event, instance) {
+  'click .traffic' (event, template) {
+    var st = Number(Session.get("strength-t"));
     if ($('.traffic').prop('checked')) {
       $(".im-t, .traffic-info, .cr-name-t, .crf-traffic, .crh-traffic, .cityrow-traffic").fadeIn();
+      Session.set("strength-t",Number(st+1));
     } else {
       $(".im-t, .traffic-info, .cr-name-t, .crf-traffic, .crh-traffic, .cityrow-traffic").fadeOut();
+      Session.set("strength-t",Number(st+1));
     }
   },
-  'click .polluted'(event, instance) {
+  'click .polluted'(event, template) {
+    var sp = Number(Session.get("strength-p"));
     if ($('.polluted').prop('checked')) {
       $(".im-p, .airq-info, .cr-name-p, .crf-polution, .crh-polution, .cityrow-polution").fadeIn();
+      Session.set("strength-p",Number(sp+1));
     } else {
       $(".im-p, .airq-info, .cr-name-p, .crf-polution, .crh-polution, .cityrow-polution").fadeOut();
+      Session.set("strength-p",Number(sp+1));
     }
   },
-  'click paper-checkbox'(event, instance) {
+  'click paper-checkbox'(event, template) {
     var count = 0;
     $('paper-checkbox[checked]').each(function() { count = count + 1; });
     if(count < 1) {
@@ -79,10 +87,17 @@ Template.navfilter.events({
       if(Session.get("option") == "map") $(".leaflet-marker-icon").fadeIn();
     }
   }
+
 });
 
 Template.navfilter.rendered = function () {
-  this.$("#health-slider").noUiSlider({
+  var health   = $("#health-slider");
+  var safety   = $("#safety-slider");
+  var polution = $("#polution-slider");
+  var traffic  = $("#traffic-slider");
+
+
+  health.noUiSlider({
     start: 80,
     connect: "lower",
     range: { 'min': 0, 'max': 100 }
@@ -90,7 +105,7 @@ Template.navfilter.rendered = function () {
     Session.set("strength-h",val);
   });
 
-  this.$("#safety-slider").noUiSlider({
+  safety.noUiSlider({
     start: 80,
     connect: "lower",
     range: { 'min': 0, 'max': 100 }
@@ -98,7 +113,7 @@ Template.navfilter.rendered = function () {
     Session.set("strength-s",val);
   });
 
-  this.$("#polution-slider").noUiSlider({
+  polution.noUiSlider({
     start: 80,
     connect: "lower",
     range: { 'min': 0, 'max': 100 }
@@ -106,11 +121,12 @@ Template.navfilter.rendered = function () {
     Session.set("strength-p",val);
   });
 
-  this.$("#traffic-slider").noUiSlider({
+  traffic.noUiSlider({
     start: 80,
     connect: "lower",
     range: { 'min': 0, 'max': 100 }
   }).on('slide', function (ev, val) {
     Session.set("strength-t",val);
   });
+
 };
--- client/routes.js
+++ client/routes.js
@@ -10,29 +10,27 @@ Router.route('/:_id', {
   data: function () {
     var courses;
     var option = Router.current().params._id;
-    Meteor.subscribe("tweets", function(){
-      Meteor.subscribe("cities", function(){
-          $(".loading-screen").fadeOut(function(){
-            $(this).remove();
-          });
-          Session.set("strength",0);
-          Session.set("strength-t",0);
-          Session.set("strength-p",0);
-          Session.set("strength-s",0);
-          Session.set("strength-h",0);
+    Meteor.subscribe("cities", function(){
+      $(".loading-screen").fadeOut(function(){
+        $(this).remove();
 
-          Session.set("option",option);
-          Session.set("qnumber",1);
+        Session.setDefault("strength-t",80);
+        Session.setDefault("strength-p",80);
+        Session.setDefault("strength-s",80);
+        Session.setDefault("strength-h",80);
+        Session.setDefault("option",option);
+        Session.setDefault("qnumber",1);
 
-          var isChrome = !!window.chrome && !!window.chrome.webstore;
-          if(isChrome) {
-            Blaze.render(Template.welcome,$(".welcome-screen")[0]);
-            if(option == "map")   Blaze.render(Template.map,$("body")[0]);
-            if(option == "chart") Blaze.render(Template.regression,$("body")[0]);
-            if(option == "dots")  Blaze.render(Template.dots,$("body")[0]);
-          } else {
-            $(".welcome-screen").text("This evaluation is only available in Google Chrome. 1.0 - 48 or above.");
-          }
+        var isChrome = !!window.chrome && !!window.chrome.webstore;
+        if(isChrome) {
+          Blaze.render(Template.welcome,$(".welcome-screen")[0]);
+          if(option == "map")   Blaze.render(Template.map,$("body")[0]);
+          if(option == "chart") Blaze.render(Template.regression,$("body")[0]);
+          if(option == "dots")  Blaze.render(Template.dots,$("body")[0]);
+        } else {
+          $(".welcome-screen").text("This evaluation is only available in Google Chrome. 1.0 - 48 or above.");
+        }
+
       });
     });
   }
